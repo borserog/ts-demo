a21ac121f40c7fc21c8c8c918ac0c20ab0a8df84
Gabriel Borsero <gabriel.borsero@gmail.com>
2024-01-06 14:58:41 -0300

    refactoring add form

--- src/app/components/add-deal-form/add-deal-form.component.ts
+++ src/app/components/add-deal-form/add-deal-form.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { dealTypes, RealStateDeal } from '../../shared/models/real-state-deal';
+import { NgClass, NgPlural, NgPluralCase } from '@angular/common';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { DealForm } from '../deal-dialog/deal-form-dialog.component';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 @Component({
   selector: 'ts-add-deal-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    NgPlural,
+    ReactiveFormsModule,
+    CdkAccordionModule,
+    NgClass,
+    NgPluralCase,
+  ],
   templateUrl: './add-deal-form.component.html',
-  styleUrl: './add-deal-dialog.component.css',
 })
-export class AddDealFormComponent {}
+export class AddDealFormComponent {
+  @Output() requestCloseDialog = new EventEmitter();
+  @Output() requestSaveNewDeals = new EventEmitter<RealStateDeal[]>();
+  fb = inject(FormBuilder);
+  dealForm: DealForm = {
+    name: ['', Validators.required],
+    address: ['', Validators.required],
+    purchasePrice: ['', Validators.required],
+    netOperationalIncome: ['', Validators.required],
+    type: ['', Validators.required],
+  };
+  form = this.fb.group({
+    dealsForm: this.fb.array([this.fb.group({ ...this.dealForm })]),
+  });
+  protected readonly dealTypes = dealTypes;
+  protected readonly Object = Object;
+
+  get dealsForm() {
+    return this.form.controls['dealsForm'] as FormArray<any>;
+  }
+
+  closeDialog() {
+    this.requestCloseDialog.emit();
+  }
+
+  castFormGroup(dealForm: any): FormGroup {
+    return dealForm as FormGroup;
+  }
+
+  saveNewDeals() {
+    this.requestSaveNewDeals.emit(
+      this.form.controls.dealsForm.value as unknown as RealStateDeal[]
+    );
+  }
+
+  addNewDealForm() {
+    this.dealsForm.push(this.fb.group({ ...this.dealForm }));
+  }
+}
--- src/app/components/deal-dialog/deal-form-dialog.component.ts
+++ src/app/components/deal-dialog/deal-form-dialog.component.ts
@@ -1,18 +1,10 @@
-import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
-import { MainComponent } from '../main/main.component';
-import {
-  FormArray,
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { JsonPipe, NgClass, NgPlural, NgPluralCase } from '@angular/common';
+import { Component, inject } from '@angular/core';
+
 import { DialogRef } from '@angular/cdk/dialog';
-import { CdkOverlayOrigin } from '@angular/cdk/overlay';
 import { RealStateDealService } from '../../shared/services/real-state-deal.service';
-import { dealTypes, RealStateDeal } from '../../shared/models/real-state-deal';
-import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { RealStateDeal } from '../../shared/models/real-state-deal';
+import { CdkOverlayOrigin } from '@angular/cdk/overlay';
+import { AddDealFormComponent } from '../add-deal-form/add-deal-form.component';
 
 export type DealForm = {
   [index in keyof Partial<RealStateDeal>]: any[];
@@ -21,67 +13,19 @@ export type DealForm = {
 @Component({
   selector: 'ts-deal-dialog',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    JsonPipe,
-    CdkOverlayOrigin,
-    CdkAccordionModule,
-    NgClass,
-    NgPlural,
-    NgPluralCase,
-  ],
+  imports: [CdkOverlayOrigin, AddDealFormComponent],
   templateUrl: './deal-form-dialog.component.html',
 })
-export class DealFormDialog implements OnInit {
+export class DealFormDialog {
   dialogRef = inject(DialogRef);
   realStateDealService = inject(RealStateDealService);
-
-  @Output() dialogClosed = new EventEmitter();
-
-  fb = inject(FormBuilder);
-
-  dealForm: DealForm = {
-    name: ['', Validators.required],
-    address: ['', Validators.required],
-    purchasePrice: ['', Validators.required],
-    netOperationalIncome: ['', Validators.required],
-    type: ['', Validators.required],
-  };
-
-  form = this.fb.group({
-    dealsForm: this.fb.array([this.fb.group({ ...this.dealForm })]),
-  });
-
-  protected readonly MainComponent = MainComponent;
-  protected readonly dealTypes = dealTypes;
-  protected readonly Object = Object;
-
-  get dealsForm() {
-    return this.form.controls['dealsForm'] as FormArray<any>;
-  }
-
-  ngOnInit() {
-    this.form.valueChanges.subscribe(() => {
-      console.log(this.form.value);
-    });
-  }
-
-  addNewDealForm() {
-    this.dealsForm.push(this.fb.group({ ...this.dealForm }));
-  }
-
-  castFormGroup(dealForm: any): FormGroup {
-    return dealForm as FormGroup;
-  }
 
   closeDialog() {
     this.dialogRef.close();
   }
 
-  saveChanges() {
-    this.realStateDealService.addNewDeals(
-      this.form.controls.dealsForm.value as any
-    );
+  saveNewDeals(newDeals: RealStateDeal[]) {
+    this.realStateDealService.addNewDeals(newDeals);
     this.dialogRef.close();
   }
 }
