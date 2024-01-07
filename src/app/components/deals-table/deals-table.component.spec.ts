957cbeeb57c3382d58476d28b81718d9c6f261f2
Gabriel Borsero <gabriel.borsero@gmail.com>
2024-01-07 20:47:51 -0300

    unit tests for table

--- src/app/components/deals-table/deals-table.component.spec.ts
+++ src/app/components/deals-table/deals-table.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DealsTableComponent } from './deals-table.component';
+import Spy = jasmine.Spy;
+import { RealEstateDeal } from '../../shared/models/real-estate-deal';
 
 describe('DealsListComponent', () => {
   let component: DealsTableComponent;
   let fixture: ComponentFixture<DealsTableComponent>;
+  let loadDealsSpy: jest.SpyInstance;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,9 +17,45 @@ describe('DealsListComponent', () => {
     fixture = TestBed.createComponent(DealsTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loadDealsSpy = jest.spyOn(component.loadDeals$, 'next');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch deal list when component initializes', (done) => {
+    const realStateDealsServiceSpy = jest.spyOn(
+      component.realStateDealsService,
+      'getRealEstateDeals'
+    );
+
+    component.dealsList$.subscribe(() => {
+      expect(realStateDealsServiceSpy).toHaveBeenCalledTimes(2);
+      done();
+    });
+
+    component.ngOnInit();
+
+    expect(loadDealsSpy).toHaveBeenCalledWith(null);
+  });
+
+  it('should reset filters', (done) => {
+    component.filtersForm.setValue({ name: '1234', type: 'Acquisition' });
+
+    component.resetFilters();
+
+    expect(component.filtersForm.get('name')?.value).toBe('');
+    expect(component.filtersForm.get('type')?.value).toBe('');
+    done();
+  });
+
+  it('should route to deal detail', (done) => {
+    const navigateSpy = jest.spyOn(component.router, 'navigate');
+
+    component.navigateToDealDetails({ id: '1' } as RealEstateDeal);
+
+    expect(navigateSpy).toHaveBeenCalledWith(['deal', '1']);
+    done();
   });
 });
