93b9964ced92efb40665762aa76b07017ea56b6b
Gabriel Borsero <gabriel.borsero@gmail.com>
2024-01-07 15:17:53 -0300

    fix responsiveness of buttons

--- src/app/components/deals-table/deals-table.component.ts
+++ src/app/components/deals-table/deals-table.component.ts
@@ -89,9 +89,9 @@ export class DealsTableComponent implements OnInit {
 
     this.filtersForm.valueChanges
       .pipe(
+        takeUntilDestroyed(this.destroyRef),
         debounceTime(500),
         distinctUntilChanged(),
-        takeUntilDestroyed(this.destroyRef),
         tap(console.log)
       )
       .subscribe((filters) => {
