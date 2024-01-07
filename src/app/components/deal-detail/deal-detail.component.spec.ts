1ff124700f43bc7859ba9e43daefe0959c9aac5d
Gabriel Borsero <gabriel.borsero@gmail.com>
2024-01-07 20:26:38 -0300

    unit tests for detail-deal

--- src/app/components/deal-detail/deal-detail.component.spec.ts
+++ src/app/components/deal-detail/deal-detail.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DealDetailComponent } from './deal-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RealEstateDeal } from '../../shared/models/real-estate-deal';
+import { Dialog } from '@angular/cdk/dialog';
+import { of } from 'rxjs';
+import spyOn = jest.spyOn;
+import { DealFormDialogComponent } from '../deal-dialog/deal-form-dialog.component';
 
 describe('DealDetailComponent', () => {
   let component: DealDetailComponent;
@@ -8,16 +15,63 @@ describe('DealDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DealDetailComponent]
-    })
-    .compileComponents();
-    
+      imports: [DealDetailComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: jest.fn() },
+        {
+          provide: Dialog,
+          useValue: {
+            open: jest.fn(() => {
+              return {
+                closed: of({}),
+              };
+            }),
+          },
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DealDetailComponent);
     component = fixture.componentInstance;
+    component.dealId = '1';
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize deal', (done) => {
+    const realsStateDealServiceSpy = jest.spyOn(
+      component.realsStateDealService,
+      'getDealById'
+    );
+
+    component.deal$.subscribe(() => done());
+
+    expect(realsStateDealServiceSpy).toHaveBeenCalledWith('1');
+  });
+
+  it('should open dialog', (done) => {
+    component.openDialog(mockedDeal as RealEstateDeal);
+    done();
+
+    expect(component.dialog.open).toHaveBeenCalledWith(
+      DealFormDialogComponent,
+      {
+        minWidth: '80vw',
+        data: { ...mockedDeal },
+      }
+    );
+  });
 });
+
+const mockedDeal = {
+  id: '21',
+  name: 'Industrial Logistics Center',
+  type: 'Lease',
+  purchasePrice: 8500000,
+  address: '789 Logistics Lane, Industriopolis',
+  netOperationalIncome: 670000,
+};
