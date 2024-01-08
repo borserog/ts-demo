a1a795bf04a58d15b3c58a20028214df4ae3d578
Gabriel Borsero <gabriel.borsero@gmail.com>
2024-01-07 21:08:43 -0300

    remove component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { MainComponent } from './components/main/main.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, MainComponent],
+  imports: [RouterModule, MainComponent],
   selector: 'ts-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
