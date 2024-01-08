66c08622d805c7a2f316418fe125c7875a0c3cc0
Gabriel Borsero <gabriel.borsero@gmail.com>
2024-01-07 21:05:24 -0300

    edit form test

--- src/app/components/edit-deal-form/edit-deal-form.component.spec.ts
+++ src/app/components/edit-deal-form/edit-deal-form.component.spec.ts
@@ -8,16 +8,59 @@ describe('EditDealFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditDealFormComponent]
-    })
-    .compileComponents();
-    
+      imports: [EditDealFormComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(EditDealFormComponent);
     component = fixture.componentInstance;
+    component.realStateDeal = {
+      id: '19',
+      name: 'Tech Valley Data Center',
+      type: 'Acquisition',
+      purchasePrice: 4800000,
+      address: '123 Data Street, Tech Valley',
+      netOperationalIncome: 320000,
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('populate form when component initializes', () => {
+    component.ngOnInit();
+
+    expect(component.form.get('name')?.value).toBe(
+      component.realStateDeal.name
+    );
+    expect(component.form.get('address')?.value).toBe(
+      component.realStateDeal.address
+    );
+    expect(component.form.get('purchasePrice')?.value).toBe(
+      component.realStateDeal.purchasePrice.toString()
+    );
+    expect(component.form.get('netOperationalIncome')?.value).toBe(
+      component.realStateDeal.netOperationalIncome.toString()
+    );
+    expect(component.form.get('type')?.value).toBe(
+      component.realStateDeal.type
+    );
+  });
+
+  it('should request dialog to be closed', () => {
+    const spy = jest.spyOn(component.requestCloseDialog, 'emit');
+
+    component.closeDialog();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should request deal to be edited', () => {
+    const spy = jest.spyOn(component.requestEditDeal, 'emit');
+
+    component.editDeal();
+
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
